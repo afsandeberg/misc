35dbe2d82a7b8e0e119112907cb00e49d85bbe5c
afsandeberg <marten.afsandeberg@gmail.com>
2017-12-09 16:09:38 +0100

    Gen 3

--- PoGo/maspogo.js
+++ PoGo/maspogo.js
@@ -544,7 +544,8 @@ MaS.PoGo.fn = (function () {
         showOnly.append('<label><input type="checkbox" value="shownotify">Notify</label>');
         showOnly.append('<label><input type="checkbox" value="1,2,3">1-Bulbasaur+</label>');
         showOnly.append('<label><input type="checkbox" value="4,5,6">4-Charmander+</label>');
-        showOnly.append('<label><input type="checkbox" value="58,59">58-Growlithe+</label>');
+        // showOnly.append('<label><input type="checkbox" value="58,59">58-Growlithe+</label>');
+        showOnly.append('<label><input type="checkbox" value="252,253,254,255,256,257,258,259,260,261,262,263,264,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,288,289,290,291,292,293,294,295,296,297,298,299,300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360,361,362,363,364,365,366,367,368,369,370,371,372,373,374,375,376,377,378,379,380,381,382,383,384,385,386">Gen 3</label>');
         showOnly.append('<label><input type="checkbox" value="63,64,65">63-Abra+</label>');
         showOnly.append('<label><input type="checkbox" value="66,67,68">66-Machop+</label>');
         showOnly.append('<label><input type="checkbox" value="74,75,76">74-Geodude+</label>');
